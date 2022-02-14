e1059659a6da8e0f13539129b51d8d59897a199a
Marcelo Silva Lima <mslmarcelosilva@gmail.com>
2022-02-13 21:06:50 -0300

    Projeto_Veiculo

---
+++ Projeto_Veiculo/Projetos_GFT/Entities/Veiculo.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto_Veiculo.Entities
+{
+    class Veiculo
+    {
+        public string Marca { get; set; }
+        public string Modelo { get; set; }
+        public string Placa { get; set; }
+        public string Cor { get; set; }
+        public string Km { get; set; }
+        public bool IsLigado { get; set; }
+        public int LitrosCombustivel { get; set; }
+        public int Velocidade { get; set; }
+        public double Preco { get; set; }
+
+        public Veiculo()
+        {
+
+        }
+
+        public Veiculo(string marca, string modelo, string placa, string cor, string km, bool isLigado, int litrosCombustivel, int velocidade, double preco)
+        {
+            Marca = marca;
+            Modelo = modelo;
+            Placa = placa;
+            Cor = cor;
+            Km = km;
+            IsLigado = isLigado;
+            LitrosCombustivel = litrosCombustivel;
+            Velocidade = velocidade;
+            Preco = preco;
+        }
+
+        public void Acelerar()
+        {
+            if (IsLigado)
+            {
+                Velocidade += 20;
+            }
+            else
+            {
+                Console.WriteLine("Ligue o carro primeiro!");
+            }
+        }
+
+        public void Abastecer(int qtd)
+        {
+            if (LitrosCombustivel + qtd > 60)
+            {
+                var excesso = 60 - LitrosCombustivel;
+                Console.WriteLine("Quantidade inserida excede o limite do tanque, permitido apenas a quantidade: " + excesso);
+            }
+            else
+            {
+                LitrosCombustivel += qtd;
+            }
+        }
+        public void Frear()
+        {
+            if (Velocidade < 20)
+            {
+                Velocidade = 0;
+            }
+            else
+            {
+                Velocidade -= 20;
+            }
+
+        }
+        public void Pintar(string cor)
+        {
+            Cor = cor;
+        }
+        public void Ligar()
+        {
+            if (!IsLigado)
+            {
+                IsLigado = true;
+                Console.WriteLine("Carro ligado com sucesso!");
+            }
+            else
+            {
+                Console.WriteLine("Carro já esta ligado!");
+            }
+        }
+        public void Desligar()
+        {
+            if (IsLigado && Velocidade < 1)
+            {
+                IsLigado = false;
+                Console.WriteLine("Carro desligado com sucesso!");
+            }
+            else if (Velocidade > 1)
+            {
+                Console.WriteLine("Veiculo não pode ser desligado, pois esta em movimento." + IsLigado);
+            }
+            else
+            {
+                Console.WriteLine("Veiculo já se se encontra desligado." + IsLigado);
+            }
+        }
+        public override string ToString()
+        {
+            return "Marca: " + Marca + System.Environment.NewLine +
+                    "Modelo: " + Modelo + System.Environment.NewLine +
+                    "Placa: " + Placa + System.Environment.NewLine +
+                    "Cor: " + Cor + System.Environment.NewLine +
+                    "Km: " + Km + System.Environment.NewLine +
+                    "Veiculo ligado: " + IsLigado + System.Environment.NewLine +
+                    "Tanque: " + LitrosCombustivel + System.Environment.NewLine +
+                    "Velocidade: " + Velocidade + System.Environment.NewLine +
+                    "Preco: " + Preco + System.Environment.NewLine;
+        }
+    }
+}
---
+++ Projeto_Veiculo/Projetos_GFT/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using Projeto_Veiculo.Entities;
+
+namespace Projeto_Veiculo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Veiculo v1 = new Veiculo("GM", "Astra", "BOT1234", "Azul", "3000", true, 10, 30, 5000.00);
+
+            Console.WriteLine("-------------------Menu de opções -------------");
+            Console.WriteLine("Opção 1: Visualizar Veiculo");
+            Console.WriteLine("Opção 2: Abastecer Veiculo");
+            Console.WriteLine("Opção 3: Acelerar Veiculo");
+            Console.WriteLine("Opção 4: Frear Veiculo");
+            Console.WriteLine("Opção 5: Pintar Veiculo");
+            Console.WriteLine("Opção 6: Desligar Veiculo");
+            Console.WriteLine("Opção 7: Ligar Veiculo");
+            Console.WriteLine("Ou tecle X para sair");
+            Console.WriteLine("Selecione a opção que deseja executar: ");
+            string opc = Console.ReadLine();
+
+            while(opc.ToUpper() != "X")
+            {
+                switch (opc)
+                {
+                    case "1":
+                        Console.WriteLine(v1);
+                        break;
+                    case "2":
+                        Console.Write("Insira a quantidade em Litros que deseja abastecer:");
+                        int abastecer = int.Parse(Console.ReadLine());
+                        v1.Abastecer(abastecer);
+                        Console.WriteLine("Tanque: " + v1.LitrosCombustivel);
+                        Console.WriteLine();
+                        break;
+                    case "3":
+                        v1.Acelerar();
+                        Console.WriteLine("Velocidade: " + v1.Velocidade);
+                        Console.WriteLine();
+                        break;
+                    case "4":
+                        v1.Frear();
+                        Console.WriteLine("Velocidade: " + v1.Velocidade);
+                        Console.WriteLine();
+                        break;
+                    case "5":
+                        Console.WriteLine("Digite a nova cor do veiculo: ");
+                        string cor = Console.ReadLine();
+                        v1.Pintar(cor.ToUpper());
+                        Console.WriteLine("Nova cor do veiculo: " + v1.Cor);
+                        Console.WriteLine();
+                        break;
+                    case "6":
+                        v1.Desligar();
+                        break;
+                    case "7":
+                        v1.Ligar();
+                        break;
+                    default:
+                        Console.WriteLine("Opção invalida, escolha uma das opções abaixo");
+                        break;
+                }
+                Console.WriteLine("-------------------Menu de opções -------------");
+                Console.WriteLine("Opção 1: Visualizar Veiculo");
+                Console.WriteLine("Opção 2: Abastecer Veiculo");
+                Console.WriteLine("Opção 3: Acelerar Veiculo");
+                Console.WriteLine("Opção 4: Frear Veiculo");
+                Console.WriteLine("Opção 5: Pintar Veiculo");
+                Console.WriteLine("Opção 6: Desligar Veiculo");
+                Console.WriteLine("Opção 7: Ligar Veiculo");
+                Console.WriteLine("Ou tecle X para sair");
+                Console.WriteLine("Selecione a opção que deseja executar: ");
+                opc = Console.ReadLine();
+            }            
+        }
+    }
+}
